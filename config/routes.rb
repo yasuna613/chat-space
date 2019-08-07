f86c224e36f17f7ab82df30bcede0fd42f1c7666
yasuna613 <yasuna.613@gmail.com>
2019-08-07 20:44:53 +0900

    追加　routes、group_controller.rb アクション

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "messages#index"
   devise_for :users
   resources :users, only: [:edit, :update]
-  
+
+  resources :groups, only: [:new, :create, :edit, :update]  
 
 end
