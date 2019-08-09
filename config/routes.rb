fdc73db6766dce753ef5e87e9e772380b71eb012
yasuna613 <yasuna.613@gmail.com>
2019-08-10 02:21:03 +0900

    add message model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
 
-  resources :groups, only: [:index, :new, :create, :edit, :update] 
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages
+  end
 
 end
