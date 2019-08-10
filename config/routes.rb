3668b3deddbeae62ea0f8a2a028f66d7e17f4815
yasuna613 <yasuna.613@gmail.com>
2019-08-10 20:59:17 +0900

    追加　メッセージ＆画像送信機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
 
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :new, :create]
   end
 
 end
