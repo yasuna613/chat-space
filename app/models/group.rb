2ce071b024843b82788cb7613aebf9674e7a8c99
yasuna613 <yasuna.613@gmail.com>
2019-08-24 22:21:26 +0900

    追加 message~テスト

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,13 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :members
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.text? ? last_message.text : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
